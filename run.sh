056765a00ccb7e65c40013ca51b4d1c4f00c16e1
saulius <noiseoverip@gmail.com>
2013-05-01 22:09:54 +0200

    Split into two tests

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+echo "Building test project with ant..."
+CURRNT_DIR=${PWD}
+
+ant build
+adb push $CURRNT_DIR/bin/OneButtonUiTest.jar /data/local/tmp/
+adb shell uiautomator runtest OneButtonUiTest.jar -c com.test.TestButton1,com.test.TestButton2
+
+exit
---
+++ src/com/test/Helper.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class Helper {
+	private UiAutomatorTestCase testcase;
+	
+	public Helper(UiAutomatorTestCase testcase) {
+		this.testcase = testcase;
+	}
+	
+	public void findAppWithName(String name, String packagename) throws UiObjectNotFoundException{
+		testcase.getUiDevice().pressBack();
+		testcase.getUiDevice().pressHome();		
+		new UiObject(new UiSelector().description("Apps")).clickAndWaitForNewWindow();			
+		new UiObject(new UiSelector().text("Apps")).click();		
+	
+		UiScrollable appViews = new UiScrollable(new UiSelector().scrollable(true));		
+		appViews.setAsHorizontalList();		
+		UiObject settingsApp = appViews.getChildByText(
+				new UiSelector().className(android.widget.TextView.class.getName()), name);
+		settingsApp.clickAndWaitForNewWindow();
+		
+		//UiObject settingsValidation = new UiObject(new UiSelector().packageName(packagename));
+		//assertTrue("Could not validate application package", settingsValidation.exists());
+	}
+	
+	public void closeApp() {
+		testcase.getUiDevice().pressBack();
+		testcase.getUiDevice().pressBack();
+	}
+}
